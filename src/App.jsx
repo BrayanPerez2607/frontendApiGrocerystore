68e2bc62498e94534c7756520a68c7e3cef19754
BrayanPerez2607 <brayperez2607@gmail.com>
2024-05-18 17:39:15 -0500

    cambios completos, las 3 entidades funcionan pero la de productos solo muestra los datos por consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
-
-import './App.css'
-
-import axios from 'axios'
-import ClientsTable from './components/ClientComponents/ClientsTable'
-import ClientsForm from './components/ClientComponents/ClientsForm'
-import ProductsForm from './components/ProductComponents/ProductsForm'
-import ProductsTable from './components/ProductComponents/ProductsTable'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import ClientsTable from './components/ClientComponents/ClientsTable';
+import ClientsForm from './components/ClientComponents/ClientsForm';
+import ProductsForm from './components/ProductComponents/ProductsForm';
+import ProductsTable from './components/ProductComponents/ProductsTable';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SalesList from "./components/SaleComponents/SalesList.jsx";
+import SaleDetails from "./components/SaleComponents/SaleDetails.jsx";
+import AddSale from "./components/SaleComponents/AddSale.jsx";
 
 function App() {
   const [clients, setClients] = useState([])
@@ -100,19 +102,27 @@ function App() {
   
 
   return (
-    <div className='App'>
-      <h1>Api Grocery Store</h1>
-      <br/>
-      <h2>List Clients</h2>
-      <ClientsTable clients={clients} onEdit={handleEditClient} onDelete={handleDeleteClient} />
-      <h2>{editingClient ? 'edit client' : 'create client'}</h2>
-      <ClientsForm onSubmit={handleCreateOrUpdateClient} initialClient={editingClient} />
-      <br/>
-      <h2>List Products</h2>
-      <ProductsTable products={products} onEdit={handleEditProduct} onDelete={handleDeleteProduct} />
-      <h2>{editingProduct ? 'edit product' : 'create product'}</h2>
-      <ProductsForm onSubmit={handleCreateOrUpdateProduct} initialProduct={editingProduct} />
-    </div>
+      <div className='App'>
+          <h1>Api Grocery Store</h1>
+          <br/>
+          <h2>List Clients</h2>
+          <ClientsTable clients={clients} onEdit={handleEditClient} onDelete={handleDeleteClient}/>
+          <h2>{editingClient ? 'edit client' : 'create client'}</h2>
+          <ClientsForm onSubmit={handleCreateOrUpdateClient} initialClient={editingClient}/>
+          <br/>
+          <h2>List Products</h2>
+          <ProductsTable products={products} onEdit={handleEditProduct} onDelete={handleDeleteProduct}/>
+          <h2>{editingProduct ? 'edit product' : 'create product'}</h2>
+          <ProductsForm onSubmit={handleCreateOrUpdateProduct} initialProduct={editingProduct}/>
+          <br/>
+          <Router>
+              <Routes>
+                  <Route exact path="/apigrocerystore/sales" element={<SalesList />} />
+                  <Route path="/" element={<AddSale />} />
+                  <Route path="/apigrocerystore/sales/:id" element={<SaleDetails />} />
+              </Routes>
+          </Router>
+      </div>
   )
 }
 
